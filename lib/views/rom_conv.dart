7ded00dba4f52d09fcf5296af8f409799085756b
nick747 <micco.mario@gmail.com>
2022-12-29 13:56:03 +0100

    Separated the main screen and added a new file

---
+++ lib/views/rom_conv.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class ArabConv extends StatefulWidget {
+  const ArabConv({super.key});
+
+  @override
+  State<ArabConv> createState() => _ArabConvState();
+}
+
+class _ArabConvState extends State<ArabConv> {
+
+  TextEditingController arabNum = TextEditingController();
+  var pressed = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Arab-Roman Converter'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: arabNum,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: arabNum.clear,
+                  icon: const Icon(Icons.clear),
+                ),
+                hintText: 'Arab Number',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.00),
+                ), 
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: const Text('Convert'),
+              onPressed: () {
+                pressed = true;
+                setState(() {});
+              },
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 3.5,
+                    color: pressed ? Colors.red : Colors.white,
+                  ),
+                  borderRadius: BorderRadius.circular(8),   
+                ),
+              child: Text(
+                pressed ? calcRoman(int.parse(arabNum.text)) : 'Insert a number',
+                style: TextStyle(
+                    fontSize: 40,
+                    fontFamily:
+                        GoogleFonts.getFont('Playfair Display').fontFamily),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+String calcRoman(input) {
+  List<int> iNum = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  List<String> rNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
+  var nRomano = '';
+
+  for (var i = 0; i < iNum.length; i++) {
+    while (iNum[i] <= input) {
+      nRomano += rNum[i];
+      input -= iNum[i];
+    }
+  }
+
+  return nRomano;
+}
